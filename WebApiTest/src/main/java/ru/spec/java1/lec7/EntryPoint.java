af87b3df1b5f4629b0ed0cba45e050c65ce9ddde
student <student@tg6>
2017-10-24 21:35:09 +0300

    Home work

--- WebApiTest/src/main/java/ru/spec/java1/lec7/EntryPoint.java
+++ WebApiTest/src/main/java/ru/spec/java1/lec7/EntryPoint.java
@@ -9,6 +9,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.std.MapProperty;
 
@@ -16,15 +18,25 @@ public class EntryPoint {
 
 	public static void main(String[] args) throws Exception {
 //		specUrls();
+//		hhApi();
 		
+		//
+		//JSoup <- https://habrahabr.ru/post/340370/
+		// select ( .comments-section .comment 
+		//    ->select (.js-score).text -> int -> Map<Int,List<String>>
+		//    ->select ( .comment__message )
+		// TODO save to file
+		
+
+	}
+
+	private static void hhApi() throws IOException, JsonParseException, JsonMappingException, MalformedURLException {
 		ObjectMapper mapper = new ObjectMapper();
 		SearchResult result = mapper
 				.readValue(new URL("https://api.hh.ru/vacancies?text=java"), 
 						SearchResult.class);
 		
 		result.getItems().forEach(item->System.out.println(item.getName()));
-		
-
 	}
 
 	private static void specUrls() throws IOException, MalformedURLException {
